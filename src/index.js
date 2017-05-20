6a8d857ca222219ba13a11f0edc26f582013c92a
pautenavidal <pautenavidal@gmail.com>
2017-05-20 12:40:39 +0200

    added base files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import App from './containers/App';
+import './styles/index.css';
+import { Provider } from 'react-redux'
+import { createStore,applyMiddleware } from 'redux'
+import reducer from './reducers'
+import 'bootstrap/dist/css/bootstrap.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import {browserHistory} from 'react-router'
+import makeRoutes from './routes'
+import thunk from 'redux-thunk'
+import {createLogger} from 'redux-logger'
+
+
+const routes = makeRoutes();
+
+const logger = createLogger();
+let store = createStore(reducer,applyMiddleware(thunk,logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+      <App history={browserHistory}
+            routes={routes} />
+  </Provider>,
+  document.getElementById('root')
+);
